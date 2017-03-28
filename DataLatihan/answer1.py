6ecce0998a23331756e130110f72095899ebf446
Hamad Fauzi Jessar <hamadfauzi23@gmail.com>
2017-03-28 16:17:55 +0700

    Update answer1.py

--- DataLatihan/answer1.py
+++ DataLatihan/answer1.py
@@ -1,9 +1,20 @@
-data1 = "Data_1.txt"
-data2 = "Data_2.txt"
+data1 = "Data1.txt"
+data2 = "Data2.txt"
 
 def readData(data1):
-	x = []
-	with open(data1) as data :
-	    for line in data :
-		    x = line.split()
-	return x
+    x = []
+    with open(data1) as data:
+        for line in data:
+            x = line.split()
+    return x
+
+x = readData(data1)
+txt = []
+for i in x:
+    if (i == 'and') or (i == "I") or (i == "The") or (i == "you"):
+        txt.append("*"*len(i))
+    else:
+        txt.append(i)
+
+txt = ' '.join(txt);
+print txt
