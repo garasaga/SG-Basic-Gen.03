8d69e6be8884f5fbfe7fd7401c2eaca823016d8d
Hamad Fauzi Jessar <hamadfauzi23@gmail.com>
2017-03-28 16:21:13 +0700

    Update

--- DataLatihan/answer2.py
+++ DataLatihan/answer2.py
@@ -1,9 +1,26 @@
-data1 = "Data_1.txt"
-data2 = "Data_2.txt"
+data1 = "Data1.txt"
+data2 = "Data2.txt"
 
 def readData(data1):
-	x = []
-	with open(data1) as data :
-	    for line in data :
-		    x = line.split()
-return x
+    x = []
+    with open(data1) as data:
+        for line in data:
+            x = line.split()
+    return x
+
+x = readData(data1)
+z = readData(data2)
+txt = []
+if len(x) > len(z):
+    for item in x:
+        j = z.count(item)
+        if j > 0:
+            txt.append(item)
+else:
+    for item in z:
+        j = x.count(item)
+        if j > 0:
+            txt.append(item);
+txt = set(txt)
+print txt
+            #set()
